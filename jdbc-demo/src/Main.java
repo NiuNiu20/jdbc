28d099eff3e3895936e4dcccc4b83bbfbde9b9b5
liheng-nn <58472261+liheng-nn@users.noreply.github.com>
2023-01-05 21:35:22 +0800

    'test1'

---
+++ jdbc-demo/src/Main.java
@@ -0,0 +1,20 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Class.forName("com.mysql.jdbc.Driver");
+        String url = "jdbc:mysql://127.0.0.1:3306/db1?useSSL=false";
+        String username = "root";
+        String password = "1234";
+        Connection conn = DriverManager.getConnection(url, username, password);
+
+        String sql = "update stu set money = 2000 where id = 1";
+        Statement statement = conn.createStatement();
+        int update = statement.executeUpdate(sql);
+        System.out.println(update);
+        statement.close();
+        conn.close();
+    }
+}
